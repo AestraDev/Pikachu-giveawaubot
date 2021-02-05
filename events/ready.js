97df2fdd872d40e00d6774352693a6ae679818a3
Pikachu <69028254+mitul009@users.noreply.github.com>
2021-02-05 17:30:58 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
   const activities = [`Giveaways in ${client.guilds.cache.size} guilds`,"g!help",`over ${client.users.cache.size} users!`];
   setInterval(() => {
     let activity = activities[Math.floor(Math.random() * activities.length)];
-    client.user.setActivity("Source by ZeroDiscord", { type: "WATCHING" });
+    client.user.setActivity(activity, { type: "WATCHING" });
   }, 20000);
 
 };
