6b5d5be61280382e53729b2691592eab2bf933d5
Pikachu <69028254+mitul009@users.noreply.github.com>
2021-02-05 17:36:36 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
 module.exports = (client) => {
   console.log(
-    `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
+    `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.guilds.cache
+            .reduce((a, b) => a + b.memberCount, 0)
+            .toLocaleString()}  users.`
   );
 
   const activities = [`Giveaways in ${client.guilds.cache.size} guilds`,"g!help",`over ${client.users.cache.size} users!`];
